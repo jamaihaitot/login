a1aadc3107f97dcb55a58c330e4e5b00cf7f4878
Josef Jamai-Haitot <jamaihaitot.j@gmail.com>
2021-10-29 12:50:28 +0200

    removing example code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { useState } from "react";
+import LoginForm from "./components/LoginForm";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const adminUser = {
+		email: "admin@admin.com",
+		password: "admin123",
+	};
+
+	const [user, setUser] = useState({ name: "", email: "" });
+	const [error, setError] = useState("");
+
+	const Login = (details) => {
+		console.log(details);
+	};
+
+	const Logout = () => {
+		console.log("Logout");
+	};
+
+	return (
+		<div className="App">
+			<NavBar />
+			Appheader
+			<LoginForm />
+		</div>
+	);
 }
 
 export default App;
